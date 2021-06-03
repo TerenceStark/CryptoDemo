dcba100de1b3fa1223ab713e9f74d29ce025cc86
TerenceStark <xiashulang@gmail.com>
2021-06-03 20:30:28 +0800

    some changes

--- CryptoDemo/src/test/java/test/RC4Test.java
+++ CryptoDemo/src/test/java/test/RC4Test.java
@@ -1,16 +1,19 @@
 package test;
 
 import crypto.assymmetry.RC4CipherService;
+import org.junit.jupiter.api.Test;
 
 import java.io.UnsupportedEncodingException;
 
+
 public class RC4Test {
-    public static void main(String[] args) throws UnsupportedEncodingException {
+    @Test
+    public void testRC4() throws UnsupportedEncodingException {
         RC4CipherService rc4CipherService = new RC4CipherService();
         String encryptedText = rc4CipherService.encrypt("testdatatestdatatestdatatestdata", "testkeytestkeytestkeytestkey");
         System.out.println(encryptedText);
 
-        String decryptedText =rc4CipherService.decrypt(encryptedText,"testkeytestkeytestkeytestkey");
+        String decryptedText = rc4CipherService.decrypt(encryptedText, "testkeytestkeytestkeytestkey");
         System.out.println(decryptedText);
     }
 }
